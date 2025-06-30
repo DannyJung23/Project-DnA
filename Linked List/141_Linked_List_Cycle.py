a84efea4cdf29ebecb1aa15370a630e4127329be
Danny <dannyjung23@gmail.com>
2025-07-01 09:41:32 +1200

    Solve 141

--- Linked List/141_Linked_List_Cycle.py
+++ Linked List/141_Linked_List_Cycle.py
@@ -1,4 +1,24 @@
 """
 Approach:
-
+Use two pointers slow and fast pointers. The slow pointer moves one node at a time and the fast pointer moves two nodes at a time.
+If there is no cycle, the fast pointer will reach None first.
+If there is a cycle, the fast pointer will eventually catch up the slow pointer inside the loop.
 """
+
+# 31ms solution - O(n)
+
+def hasCycle(self, head):
+    if head == None:
+        return False
+    
+    slow_pointer = head
+    fast_pointer = head
+
+    while fast_pointer and fast_pointer.next:
+        slow_pointer = slow_pointer.next
+        fast_pointer = fast_pointer.next.next
+        
+        if fast_pointer == slow_pointer:
+            return True
+        
+    return False
