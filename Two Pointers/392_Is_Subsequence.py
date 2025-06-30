ea1b21e1fe31672fd776ed68eec9a3d8ba347701
Danny <dannyjung23@gmail.com>
2025-06-30 17:45:56 +1200

    Add Solution 392

--- Two Pointers/392_Is_Subsequence.py
+++ Two Pointers/392_Is_Subsequence.py
@@ -21,3 +21,16 @@ def isSubsequence(self, s, t):
                 return True
             
     return False
+
+
+# 0ms solution - O(n)
+
+def isSubsequence(self, s, t):
+    i, j = 0, 0
+
+    while i < len(s) and j < len(t):
+        if s[i] == t[j]:
+            i += 1
+        j += 1
+
+    return i == len(s)
