46a1a57d02ad1497520b405d7aa6cf2cc22be1ab
Danny <dannyjung23@gmail.com>
2025-07-04 14:42:27 +1200

    Update Approach 530

--- Binary Search Tree/530_Minimum_Absolute_Difference_in_BST.py
+++ Binary Search Tree/530_Minimum_Absolute_Difference_in_BST.py
@@ -1,5 +1,5 @@
 """
 Approach:
-Perform tree sort by in-order traversal in the BST.
+Perform in-order traversal in the BST which starts at the smallest node and sequentially visiting the next smallest node.
 Get the minimum difference between each adjacent node pair.
 """
