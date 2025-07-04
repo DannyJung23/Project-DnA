7204946617adcb66619cf9614f3725072fc55676
Danny <dannyjung23@gmail.com>
2025-07-04 15:04:12 +1200

    Add Solution 530

--- Binary Search Tree/530_Minimum_Absolute_Difference_in_BST.py
+++ Binary Search Tree/530_Minimum_Absolute_Difference_in_BST.py
@@ -1,5 +1,26 @@
 """
 Approach:
-Perform in-order traversal in the BST which starts at the smallest node and sequentially visiting the next smallest node.
+Implement recursive DFS in-order traversal in the BST which starts at the smallest node and sequentially visiting the next smallest node.
 Get the minimum difference between each adjacent node pair.
 """
+
+# 7ms solution - O(n)
+
+def getMinimumDifference(self, root):
+    self.min_diff = float('inf')
+    self.prev = None
+
+    def dfs(node):
+        if node is None:
+            return
+        
+        dfs(node.left)
+
+        if self.prev is not None:
+            self.min_diff = min(self.min_diff, node.val - self.prev)
+        self.prev = node.val
+        
+        dfs(node.right)
+    
+    dfs(root)
+    return self.min_diff
