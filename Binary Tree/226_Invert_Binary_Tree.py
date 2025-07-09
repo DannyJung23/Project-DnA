803731dffc98770dcf67501a90bc1582de3a4658
Danny <dannyjung23@gmail.com>
2025-07-09 16:25:33 +1200

    Update 226

--- Binary Tree/226_Invert_Binary_Tree.py
+++ Binary Tree/226_Invert_Binary_Tree.py
@@ -3,3 +3,17 @@ Approach:
 Swap the left and right children of all nodes in the original tree.
 Use recursion on the left and right subtrees and swap from the bottom.
 """
+
+# 0ms solution - O(n)
+
+def invertTree(self, root):
+    if root is None:
+        return None
+    
+    left = self.invertTree(root.left)
+    right = self.invertTree(root.right)
+
+    root.left = right
+    root.right = left
+
+    return root
