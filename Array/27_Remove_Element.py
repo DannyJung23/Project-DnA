5f416d026b6ce4999a637143e0a74f73adc22694
Danny <dannyjung23@gmail.com>
2025-07-27 21:21:50 +1200

    Update 27

--- Array/27_Remove_Element.py
+++ Array/27_Remove_Element.py
@@ -4,3 +4,15 @@ Make a pointer for the next non-val element.
 Go through every element of the array and if it's not the value to remove, assign it to the pointer and increment the pointer by 1.
 The pointer ends up being the count of elements that are not equal to the value to remove.
 """
+
+# 0ms solution - O(n)
+
+def removeElement(self, nums, val):
+    k = 0
+    
+    for i in range(len(nums)):
+        if nums[i] != val:
+            nums[k] = nums[i]
+            k += 1
+    
+    return k
