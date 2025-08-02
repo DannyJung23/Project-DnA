ce0172d5442eae57be66f53e64ada5cfdfa770f0
Danny <dannyjung23@gmail.com>
2025-08-02 21:27:46 +1200

    Update 637

--- Binary Tree/637_Average_of_Levels_in_Binary_Tree.py
+++ Binary Tree/637_Average_of_Levels_in_Binary_Tree.py
@@ -8,3 +8,25 @@ Algorithm:
 BFS
 """
 
+# 1ms solution - O(n)
+
+def averageOfLevels(self, root):
+    average_values = []
+    queue = [root]
+
+    while queue:
+        num_of_nodes = len(queue)  # number of nodes in the current level
+        sum_of_node_vals = 0  # sum of the values of the nodes in the current level
+
+        for i in range(num_of_nodes):
+            node = queue.pop(0)
+            sum_of_node_vals += node.val
+
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+        
+        average_values.append(float(sum_of_node_vals) / num_of_nodes)
+    
+    return average_values
