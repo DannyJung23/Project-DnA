86a06aad12d8dad135fefeb059de4b4d53acba30
Danny <dannyjung23@gmail.com>
2025-08-17 21:21:39 +1200

    Solve 35

--- Binary Search/35_Search Insert Position.py
+++ Binary Search/35_Search Insert Position.py
@@ -6,3 +6,23 @@ Iterate through a while loop until the index of the low pointer exceeds the inde
 Calculate the middle index in each iteration and check if it's the target number.
 If not, update either low or high to focus on the correct half.
 """
+
+# 0ms solution - O(logn)
+
+def searchInsert(self, nums, target):
+    low = 0
+    high = len(nums) - 1
+    
+    while high - low >= 0:
+        middle = low + ((high - low) // 2)
+
+        if nums[middle] == target:
+            return middle
+
+        elif nums[middle] > target:
+            high = middle - 1
+
+        else:
+            low = middle + 1
+            
+    return low
